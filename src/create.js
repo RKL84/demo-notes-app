9891e30a0aaa1cc33a8ba77e9bf3c1b34b084826
Rakesh L <rakeshl84@outlook.com>
2021-09-08 12:47:43 +1000

    Adding a billing API

--- src/create.js
+++ src/create.js
@@ -4,11 +4,11 @@ import dynamoDb from "./util/dynamodb";
 
 export const main = handler(async (event) => {
     const data = JSON.parse(event.body);
-    const params = {    
+    const params = {
         TableName: process.env.TABLE_NAME,
         Item: {
             // The attributes of the item to be created
-            userId: "123", // The id of the author
+            userId: event.requestContext.authorizers.iam.cognitoIdentity.identityId, // The id of the author
             noteId: uuid.v1(), // A unique uuid
             content: data.content, // Parsed from request body
             attachment: data.attachment, // Parsed from request body
